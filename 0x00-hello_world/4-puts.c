4a63534850d8936dc046a07e129fa2aa202526c4
balkissty <ogundotunbalkiss@gmail.com>
2022-06-02 14:02:02 -0700

    d

--- 0x00-hello_world/4-puts.c
+++
@@ -1,9 +0,0 @@
-/**
- *  * main - prints "Programming is like building a multilingual puzzle
- *   * Return: 0
- *    */
-int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
